05398b29d1c7f4994c334e3bd980dfd1b6903a46
carlomonterrey <carlosmonterrey17@gmail.com>
2023-06-12 13:14:11 -0400

    add some style to the section 2 in the transform view

--- src/app/data-transformation/data-transformation.component.ts
+++ src/app/data-transformation/data-transformation.component.ts
@@ -1,58 +1,56 @@
-import { BreakpointObserver, Breakpoints, MediaMatcher } from "@angular/cdk/layout";
-import { Component } from "@angular/core";
-import { Observable } from "rxjs/internal/Observable";
-import { map } from "rxjs/operators";
+import {
+  BreakpointObserver,
+  Breakpoints,
+  MediaMatcher,
+} from "@angular/cdk/layout";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-data-transformation",
   templateUrl: "./data-transformation.component.html",
   styleUrls: ["./data-transformation.component.scss"],
 })
-export class DataTransformationComponent {
+export class DataTransformationComponent implements OnInit{
   mobileQuery: MediaQueryList;
   currentBreakpoint: string;
-  cols: number = 2;
-  public buttons_array:any[]=[{label:"GUARDAR",color:"basic"},{label:"CANCELAR",color:"primary"},{label:"TRANSFORMAR",color:"accent"}]
+  cols_section1: number = 2;
+  cols_section2: number = 3;
+  colspan_section2: number = 1;
+  public buttons_array: any[] = [
+    { label: "GUARDAR", color: "basic" },
+    { label: "CANCELAR", color: "primary" },
+    { label: "TRANSFORMAR", color: "accent" },
+  ];
 
-
-
-
-
-  constructor(private breakpointObserver: BreakpointObserver,private mediaMatcher: MediaMatcher) {
-    this.mobileQuery = this.mediaMatcher.matchMedia('(max-width: 599.98px)');
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private mediaMatcher: MediaMatcher
+  ) {
+    this.mobileQuery = this.mediaMatcher.matchMedia("(max-width: 599.98px)");
     this.currentBreakpoint = this.getBreakpoint();
   }
 
-
   ngOnInit() {
-this.addGridStyle()
-
-
+    this.addGridStyle();
   }
 
-/**
- * check if it is mobile view, if so, assign 1 to my column variable and
- * to the height of my rows of 30 em and so on with all my breakpoints
- *
- */
-  public addGridStyle(){
-    if (this.getBreakpoint() === 'Handset') {
-      this.cols = 1;
-
-
-    } else if (this.getBreakpoint()  === 'Tablet') {
-      console.log(1);
-      this.cols = 1;
-
+  /**
+   * check if it is mobile view, if so, assign 1 to my column variable and
+   * to the height of my rows of 30 em and so on with all my breakpoints
+   *
+   */
+  public addGridStyle() {
+    if (this.getBreakpoint() === "Handset") {
+      this.cols_section1 = 1;
+      this.cols_section2 = 1;
+    } else if (this.getBreakpoint() === "Tablet") {
+      this.cols_section1 = 2;
+      this.cols_section2 = 3;
     } else {
       console.log(2);
-      this.cols = 2;
-
-
+      this.cols_section1 = 2;
+      this.colspan_section2 = 2;
     }
-
-
-
   }
   /**
    * get the name of the current breakpoint based on the screen size
@@ -60,12 +58,13 @@ this.addGridStyle()
    */
   public getBreakpoint(): string {
     if (this.mobileQuery.matches) {
-      return 'Handset';
+      return "Handset";
     } else if (this.breakpointObserver.isMatched(Breakpoints.Tablet)) {
-      return 'Tablet';
+      return "Tablet";
     } else if (this.breakpointObserver.isMatched(Breakpoints.Web)) {
-      return 'Web';
+      return "Web";
     } else {
-      return 'Unknown';
-    }}
+      return "Unknown";
+    }
+  }
 }
