ce21feaffbaa4383c231f356ef3033ae70ad4dbc
carlomonterrey <carlosmonterrey17@gmail.com>
2023-10-28 12:13:45 -0400

    Fix(upload-widget component )fix the validation logic of the component

--- src/app/components/data-transformation/upload-widget/upload-widget.component.ts
+++ src/app/components/data-transformation/upload-widget/upload-widget.component.ts
@@ -8,43 +8,46 @@ import { ConfigurationJsonService } from 'src/services/configuration-json.servic
 })
 export class UploadWidgetComponent {
   files: File[] = [];
-constructor(private configurationService: ConfigurationJsonService) { }
+  constructor(private configurationService: ConfigurationJsonService) { }
+  /** 
+   * This method handles the selection of a file and performs a validation process on it. 
+   * It saves the first selected file in the  selectedFile  variable and waits for the  processFile  method to validate the file. 
+   * If the file is valid, it adds it to the  files  array. Otherwise, it logs an error message. 
+   *  
+   * @param event The event containing the selected file(s) 
+   */
   async onSelect(event) {
-    console.log(event);
-    this.files.push(...event.addedFiles);
-    await this.processFile();
-    
+    const selectedFile = event.addedFiles[0]
+    console.log("12", selectedFile);
+    const isValidFile = await this.processFile(selectedFile);
+    if (isValidFile) {
+      this.files.push(selectedFile);
+    } else {
+      console.error("Invalid file selected.");
+    }
+
   }
-  
+
   onRemove(event) {
     console.log(event);
     this.files.splice(this.files.indexOf(event), 1);
   }
 
-  onSave(event){
-    console.log(this.files);
- 
-  }
-  // Process the selected file
-async processFile() {
-  // Get the first selected file
-  let selectedFile = this.files[0];
-  
-  // Validate that the file has a .json extension
-  if (selectedFile.name.endsWith('.json')) {
-    // Read the content of the file
+  onSave() {
+    let selectedFile = this.files[0];
     const fileReader = new FileReader();
+    // Read the content of the file
     fileReader.onload = (e) => {
       try {
         // Parse the file content as JSON
         const parsedData = JSON.parse(fileReader.result as string);
-       //Set the parsed  JSON data in the ConfigurationJsonService
+        //Set the parsed  JSON data in the ConfigurationJsonService
         this.configurationService.setConfigurationJson(parsedData);
         // The 'parsedData' now contains the parsed JSON object from the file
         console.log('Valid JSON file:', parsedData);
         this.configurationService.getConfigurationJson().subscribe(json => {
-          console.log("asdsd",json);
-          
+          console.log("asdsd", json);
+
           // Realiza acciones adicionales con el JSON de configuración
         });
       } catch (error) {
@@ -53,9 +56,21 @@ async processFile() {
       }
     };
     fileReader.readAsText(selectedFile);
-    
-  } else {
-    console.error('The file does not have a .json extension');
+
+
+  }
+  // Process the selected file
+  async processFile(selected: any): Promise<boolean> {
+    // Get the first selected file
+    let selectedFile = selected;
+
+    // Validate that the file has a .json extension
+    if (selectedFile.name.endsWith('.json')) {
+
+      return true
+    } else {
+      console.error('The file does not have a .json extension');
+      return false
+    }
   }
 }
-  }
