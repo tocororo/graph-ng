c443818caa48ca52696d85371b86eee0fe3dff80
carlomonterrey <carlosmonterrey17@gmail.com>
2023-11-17 09:24:40 -0500

    Fix (sparql query service) Fix the getquery method,working OK

--- src/app/sparql-query-layout/query-view/sparql-input/sparql-input.component.ts
+++ src/app/sparql-query-layout/query-view/sparql-input/sparql-input.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SparqlService } from 'src/services/sparql.service';
 
 @Component({
   selector: 'app-sparql-input',
@@ -9,11 +10,12 @@ export class SparqlInputComponent {
   editorOptions = {theme: 'vs-light', language: 'sparql'};
   code: string = 'SELECT * WHERE { ?s ?p ?o . }';
   originalCode: string = 'function x() { // TODO }';
-
+constructor(private sparql_service:SparqlService){}
 
 
     getQuery(){
       console.log(this.code,"code");
+      this.sparql_service.getQuery(this.code)
       
     }
 }
--- src/services/sparql.service.ts
+++ src/services/sparql.service.ts
@@ -1,9 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SparqlService {
+  URL_DEL_BACKEND= "https://127.0.0.1:5000/api/graph/sparql"
+  private query_response$: BehaviorSubject<any> = new BehaviorSubject<any>(null);
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  getQueryResponse(): Observable<any> {
+    return this.query_response$.asObservable();
+  }
+
+  setQueryResponse(response:any): void {
+    this.query_response$.next(response);
+    
+  }
+
+  getQuery(query: string) {
+    const formData = new FormData();
+    
+    formData.append('query', (query));
+    console.log(formData.get('query'));
+    this.http.post(this.URL_DEL_BACKEND, formData)
+      .subscribe(
+        response => {
+          // Lógica para manejar la respuesta del backend
+          console.log('Respuesta del backend:', response);
+          this.setQueryResponse(response)
+        },
+        error => {
+          // Lógica para manejar errores
+          console.error('Error en la solicitud:', error);
+        }
+      );
+
+  }
 }
