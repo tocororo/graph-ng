9fae30ffd98fa38b80b55c87c8a63aaff2495eba
carlomonterrey <carlosmonterrey17@gmail.com>
2023-11-17 12:36:16 -0500

    Fix (sparql query service) Working in display the nodes and the relationship in the ngx graph

--- src/app/sparql-query-layout/query-view/query-result/query-result.component.ts
+++ src/app/sparql-query-layout/query-view/query-result/query-result.component.ts
@@ -1,10 +1,135 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Node, Edge } from '@swimlane/ngx-graph';
+import { SparqlService } from 'src/services/sparql.service';
 
 @Component({
   selector: 'app-query-result',
   templateUrl: './query-result.component.html',
   styleUrls: ['./query-result.component.scss']
 })
-export class QueryResultComponent {
+export class QueryResultComponent implements OnInit {
+  nodes: Node[] = [
+    {
+      id: 'first',
+      label: 'hagagabab'
+    }, {
+      id: 'second',
+      label: 'B'
+    }, {
+      id: 'third',
+      label: 'C'
+    }, {
+      id: 'four',
+      label: 'd'
+    }
+  ]
+  links: Edge[]=[]
 
+  constructor(private sparql_service: SparqlService) {
+
+  }
+  public ngOnInit(): void {
+    this.sparql_service.getQueryResponse().subscribe((response) => {
+      if (response) {
+        const results: Array<any> = response.results
+        this.nodes = []
+        this.links=[]
+        console.log(results);
+        results.forEach((element) => {
+          if (element) {
+            if (element[0]) {
+              console.log("element[0]", element[0]);
+
+              this.addNode(element[0])
+              if (element[2]) {
+                this.addNode(element[2])
+              this.addLink(results.indexOf(element).toString(),element[0],element[2],element[1])
+
+                
+              }
+
+            }     }
+          
+
+        }
+
+        );
+        results.forEach(element => {
+            if (element[3]) {
+              console.log(this.links,"links");
+
+           /*    this.links=[]
+              this.addLink(results.indexOf(element).toString(),element[0],element[3],element[2]) */
+              
+            
+
+          }
+
+
+
+
+        })
+
+    
+  }
+
+    })
+  }
+
+
+  addNode(id_node: string): void {
+    const new_node: Node = {
+      id: '',
+      label: ''
+    };
+    new_node.id = id_node;
+    new_node.label = id_node;
+  
+    try {
+      // Validar que el id del nodo no esté vacío
+      if (new_node.id === "") {
+        throw new Error("El id del nodo no puede estar vacío");
+      }
+      // Validar que el id del nodo no exista ya
+      if (this.nodes.find(node => node.id === new_node.id)) {
+        console.log("El id del nodo ya existe");
+      }
+      // Añadir el nodo a la lista de nodos
+      this.nodes.push(new_node);
+    } catch (error) {
+      // Manejar el error
+      console.error(error);
+      // Realizar cualquier acción adicional necesaria, como mostrar un mensaje de error al usuario
+    }
+  }
+  addLink(id_link: string, source: string, target: string, label_link: string): void {
+    const new_link: Edge = {
+      id: '',
+      source: '',
+      target: '',
+      label: ''
+    };
+    new_link.id = id_link;
+    new_link.source = source;
+    new_link.target = target;
+    new_link.label = label_link;
+  
+    try {
+      // Validar que el id del enlace no esté vacío
+      if (new_link.id === '') {
+        throw new Error('El id del enlace no puede estar vacío');
+      }
+      // Validar que el id del enlace no exista ya
+      if (this.links.find(link => link.id === new_link.id)) {
+        throw new Error('El id del enlace ya existe');
+      }
+      // Añadir el enlace a la lista de enlaces
+      this.links.push(new_link);
+      console.log('============================', this.links);
+    } catch (error) {
+      // Manejar el error
+      console.error(error);
+      // Realizar cualquier acción adicional necesaria, como mostrar un mensaje de error al usuario
+    }
+  }
 }
--- src/app/sparql-query-layout/query-view/sparql-input/sparql-input.component.ts
+++ src/app/sparql-query-layout/query-view/sparql-input/sparql-input.component.ts
@@ -8,7 +8,7 @@ import { SparqlService } from 'src/services/sparql.service';
 })
 export class SparqlInputComponent {
   editorOptions = {theme: 'vs-light', language: 'sparql'};
-  code: string = 'SELECT * WHERE { ?s ?p ?o . }';
+  code: string = 'SELECT ?s ?p ?o WHERE { ?s ?p ?o FILTER (?o = "active") }';
   originalCode: string = 'function x() { // TODO }';
 constructor(private sparql_service:SparqlService){}
 
